957ad6e08b32f931718b288ab89507463f6076c6
Adrian Iloaia <bd9876633@gmail.com>
2025-08-19 12:22:51 -0700

    Use JS sitemap and remove TS config

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,12 +1,12 @@
 // app/sitemap.js
-export const dynamic = 'force-static'; // îl face static, fără surprize
-
 export default function sitemap() {
-  const base = 'https://www.elitecoatinteriors.co.uk'; // cu www!
-  const now = new Date().toISOString();
-
+  const base = 'https://www.elitecoatinteriors.co.uk'
   return [
-    { url: `${base}/`, lastModified: now },
-    // când ai pagini noi, le adaugi aici
-  ];
+    {
+      url: `${base}/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
 }
